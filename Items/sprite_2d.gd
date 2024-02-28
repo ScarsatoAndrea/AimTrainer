4457a27d045da82a6b346ee7f825ccca289121fe
andrea <andrea.scarsato@gmail.com>
2024-02-28 22:29:27 +0100

    feat: make target clickable and set lives to three

--- Items/sprite_2d.gd
+++ Items/sprite_2d.gd
@@ -1,6 +1,5 @@
 extends Sprite2D
 var speed = 0.5
-signal died
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +9,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.scale += Vector2(speed * delta, speed * delta)
-	if (self.scale.x >= 1.2):
+	if (self.scale.x >= 0.9):
 		speed = -(speed)
 	if (self.scale.x <= 0):
-		died.emit()
+		$"../../SignalManager".died.emit()
 		self.queue_free()
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_RIGHT:
+		if get_rect().has_point(to_local(event.position)):
+			$"../../SignalManager".hit.emit()
+			self.queue_free()
+			print("colpito")
--- scenes/Game.gd
+++ scenes/Game.gd
@@ -6,7 +6,9 @@ var random = RandomNumberGenerator.new()
 var lives = 3
 var time : int = 1;
 var trashold = 10;
-@onready var element = load("res://Items/circle.tscn")
+var points = 0;
+var can_start : bool = true
+@onready var element = preload("res://Items/circle.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_timer(speed)
@@ -14,11 +16,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if time >= trashold and speed > 0:
-		speed -= 0.3
+		speed -= 0.22
 		set_timer(speed)
 		time += 1
-		trashold += 15
-	print (time)
+		trashold += 7
 
 func set_timer(new_time):
 	print("parito")
@@ -27,17 +28,28 @@ func set_timer(new_time):
 	$Timer.start()
 
 
-func _on_circle_died():
+func on_circle_died():
+	lives -= 1
+	if lives == 0:
+		$Node.queue_free()
+		can_start = false
+		$Timer.stop()
 	print("morto")
-	$Circle.queue_free()
 
 
 func _on_timer_timeout():
-	print("nato")
-	time += round($Timer.wait_time)
-	x = (random.randi() % 818) + 167
-	y = (random.randi() % 314) + 167
-	var figa = element.instantiate()
-	figa.position = Vector2(x, y)
-	add_child(figa)
-	print (x) # Replace with function body.
+	if can_start:
+		print("nato")
+		time += round($Timer.wait_time)
+		x = (random.randi() % 818) + 167
+		y = (random.randi() % 314) + 167
+		var figa = element.instantiate()
+		figa.position = Vector2(x, y)
+		$Node.add_child(figa)
+		$SignalManager.connect("died", Callable(self, "on_circle_died"))
+		$SignalManager.connect("hit", Callable(self, "on_circle_hit"))
+
+
+func on_circle_hit():
+	print("ok")
+	points += 10# Replace with function body.
