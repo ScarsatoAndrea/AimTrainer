80cc68b0825b688b0f4646520f19626587eb9177
andrea <andrea.scarsato@gmail.com>
2024-02-28 19:23:49 +0100

    Add sprites spawning

--- Items/sprite_2d.gd
+++ Items/sprite_2d.gd
@@ -1,17 +1,17 @@
 extends Sprite2D
-var life_time = 1
+var speed = 0.5
 signal died
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.scale = Vector2(0,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.scale += Vector2(life_time, life_time) * delta
-	print (life_time , " " , self.scale)
-	if (self.scale.x >= 3):
-		life_time = -(life_time)
+	self.scale += Vector2(speed * delta, speed * delta)
+	if (self.scale.x >= 1.2):
+		speed = -(speed)
 	if (self.scale.x <= 0):
 		died.emit()
+		self.queue_free()
--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,25 +1,43 @@
 extends Node2D
-var speed = 10
+var speed = 1
 var x
 var y
 var random = RandomNumberGenerator.new()
-@onready var element = load("res://Items/sprite_2d.gd")
+var lives = 3
+var time : int = 1;
+var trashold = 10;
+@onready var element = load("res://Items/circle.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	set_timer(speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	x = random.randi() % 100
-	y = random.randi() % 100
-	var figa = element.instantiate()
-	figa.x = x
-	figa.y = y
-	add_child(figa)
-	print (x)
+	if time >= trashold and speed > 0:
+		speed -= 0.3
+		set_timer(speed)
+		time += 1
+		trashold += 15
+	print (time)
+
+func set_timer(new_time):
+	print("parito")
+	$Timer.autostart = true
+	$Timer.wait_time = new_time
+	$Timer.start()
 
 
 func _on_circle_died():
 	print("morto")
 	$Circle.queue_free()
+
+
+func _on_timer_timeout():
+	print("nato")
+	time += round($Timer.wait_time)
+	x = (random.randi() % 818) + 167
+	y = (random.randi() % 314) + 167
+	var figa = element.instantiate()
+	figa.position = Vector2(x, y)
+	add_child(figa)
+	print (x) # Replace with function body.
